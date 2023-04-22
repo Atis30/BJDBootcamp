77a145c440909ae3135f7e696a726f57b668a240
Atish Londhe <l.atishd@gmail.com>
2023-04-22 09:35:03 +0530

    Inital Commit

---
+++ LearnSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.prophedatum.learnspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringbootApplication.class, args);
+	}
+
+}
